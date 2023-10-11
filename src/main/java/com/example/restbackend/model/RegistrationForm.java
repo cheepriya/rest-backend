4e3f4f53777104886e919582403b6070039350d6
Administrator <priyankapinky9999@icloud.com>
2023-10-11 14:25:17 +0000

    Commit team project

---
+++ src/main/java/com/example/restbackend/model/RegistrationForm.java
@@ -0,0 +1,19 @@
+package com.example.restbackend.model;
+
+public class RegistrationForm {
+    private String Medical_History;
+
+    private String Reason_for_the_Visit;
+
+    private String List_any_current_past_medical_problems;
+
+    private String Current_Medical_Dosages;
+
+    private String Previous_gp_Details;
+
+    private String Name;
+
+    private String Address;
+
+    private String Preferred_Pharmacy;
+}
