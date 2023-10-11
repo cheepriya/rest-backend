8355159545436da3d53222bcda15b7a4ebba6979
user <mythiliramachandran@gmail.com>
2023-10-11 14:52:47 +0000

    Created controller and repository for RegistrationForm, Part1

---
+++ src/main/java/com/example/restbackend/controller/ProjectController.java
@@ -0,0 +1,22 @@
+package com.example.restbackend.controller;
+
+import com.example.restbackend.model.RegistrationForm;
+import com.example.restbackend.repository.RegistrationFormRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProjectController {
+
+    @Autowired
+    private RegistrationFormRepository regFormRepository;
+
+    @GetMapping("/registration_page")
+    public List<RegistrationForm> registrationDetails(){
+        return regFormRepository.findAll();
+    }
+
+}
