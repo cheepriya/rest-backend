5b057420434b981559bed2a896cfe8cebcba4042
user <mythiliramachandran@gmail.com>
2023-10-17 14:33:37 +0000

    created PostMapping, index html file and verified "http://localhost:8080/index.html" - Works fine

--- src/main/java/com/example/restbackend/controller/ProjectController.java
+++ src/main/java/com/example/restbackend/controller/ProjectController.java
@@ -2,10 +2,12 @@ package com.example.restbackend.controller;
 
 import com.example.restbackend.model.RegistrationForm;
 import com.example.restbackend.repository.RegistrationFormRepository;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Ref;
@@ -22,10 +24,20 @@ public class ProjectController {
         return registrationFormRepository.findAll();
     }
 
-    @PostMapping("/create")
-    public RegistrationForm createRegForm(@RequestBody RegistrationForm registrationForm){
-        RegistrationForm save = registrationFormRepository.save(registrationForm);
-        return save;
+    @PostMapping("/registration_page/create")
+    public ResponseEntity<RegistrationForm> createRegForm(@RequestBody RegistrationForm registrationForm) {
+        // Validate the request, handle data, and save to the database
+        if (registrationForm == null) {
+            // Handle invalid request
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        // You may want to add validation for the fields here
+
+        RegistrationForm savedForm = registrationFormRepository.save(registrationForm);
+
+        // Return the saved object along with a 201 Created status
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedForm);
     }
 
 }
---
+++ src/test/java/com/example/restbackend/RestBackendApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.restbackend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestBackendApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
